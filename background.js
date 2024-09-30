054f7cdceebd5918fb3e49fc2b751bb6b724a5ab
Yaminikanta-code <yaminikantaprof1@gmail.com>
2024-10-01 00:25:11 +0530

    add codes

---
+++ background.js
@@ -0,0 +1,15 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    if (e.target.id === 'grey') {
+      body.style.background = e.target.id;
+    } else if (e.target.id === 'white') {
+      body.style.background = e.target.id;
+    } else if (e.target.id === 'blue') {
+      body.style.background = e.target.id;
+    } else if (e.target.id === 'yellow') {
+      body.style.background = e.target.id;
+    }
+  });
+});
